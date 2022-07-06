80be88fe1ed455a1e9e747f7092fb28c4910d66a
Erick Saraiva <erick.ssilva@bandtec.com.br>
2022-07-06 11:06:36 -0300

    feat: adding comments about fases of the project

--- ConversorTemp/src/main/kotlin/application/ConversorServiceApplication.kt
+++ ConversorTemp/src/main/kotlin/application/ConversorServiceApplication.kt
@@ -1,4 +1,20 @@
 package application
     fun main() {
-        println("Hello World!")
+
+        /* 1 Fase do projeto é fazer funcionar via Console.
+
+           2 Fase do projeto é inserir as conversões realizadasem um banco de dados de início MySql por ser local,
+           depois tentar usar o MongoDb para aprender.
+
+           3 Fase é implementar para web, localhost gerando com uma tela feia mesmo só para análise.
+
+           4 Fase seria implementar endpoints para separar as abas por conversão especifica:
+
+           Exemplo: localhost:8080/conversor/celsius (abre o conversor para Celsius)
+           Exemplo: localhost:8080/conversor/farenheit (abre o conversor para Farenheit)
+           Exemplo: localhost:8080/conversor/kelvin (abre o conversor para Kelvin)
+
+        */
+
+        println("Hello")
     }
